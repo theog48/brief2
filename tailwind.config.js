5a3955cadfa9e473fe600865671d35d7e04a7e68
theog48 <theo.grasset.dwwm48@gmail.com>
2025-01-16 12:03:02 +0100

    Update squelette du projet

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html', // Chemin vers le fichier HTML principal
+    './src/**/*.{html,js}',  // Chemins vers les fichiers HTML et JS à analyser
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
